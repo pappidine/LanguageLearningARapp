a79011bd2e36bf710eb11dc4dce1ffc6bd633218
asfemi <pearlcstall@gmail.com>
2023-07-16 09:41:02 +0100

    populated the utilities class

--- testproject/lib/utilities.dart
+++ testproject/lib/utilities.dart
@@ -1,1 +1,28 @@
+import 'package:testproject/model.dart';
 
+class Utilities {
+  static List<QuestionsModel> QuestionsContentList() {
+    return [
+      QuestionsModel(
+        answer: 'Paris',
+        question: 'What is the capital of France?',
+      ),
+      QuestionsModel(
+        answer: 'Spanish',
+        question: 'Which language is spoken in Brazil?',
+      ),
+      QuestionsModel(
+        answer: 'yen',
+        question: 'What is the currency of Japan?',
+      ),
+      QuestionsModel(
+        answer: 'Leonardo da vinci',
+        question: 'Who painted the Mona Lisa?',
+      ),
+      QuestionsModel(
+        answer: 'Mars',
+        question: 'Which planet is known as the Red Planet?',
+      ),
+    ];
+  }
+}
